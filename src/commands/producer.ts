c47d18bfb2685848f7473b0083934fd9284b147a
Kimmo Sääskilahti <kimmo@meeshkan.com>
2020-02-05 10:17:14 +0200

    Fix style.

--- src/commands/producer.ts
+++ src/commands/producer.ts
@@ -1,25 +1,25 @@
-import {Command, flags} from '@oclif/command'
+import { Command, flags } from "@oclif/command";
 
 export default class Producer extends Command {
-  static description = 'describe the command here'
+  static description = "describe the command here";
 
   static flags = {
-    help: flags.help({char: 'h'}),
+    help: flags.help({ char: "h" }),
     // flag with a value (-n, --name=VALUE)
-    name: flags.string({char: 'n', description: 'name to print'}),
+    name: flags.string({ char: "n", description: "name to print" }),
     // flag with no value (-f, --force)
-    force: flags.boolean({char: 'f'}),
-  }
+    force: flags.boolean({ char: "f" }),
+  };
 
-  static args = [{name: 'file'}]
+  static args = [{ name: "file" }];
 
-  async run() {
-    const {args, flags} = this.parse(Producer)
+  async run(): Promise<void> {
+    const { args, flags } = this.parse(Producer);
 
-    const name = flags.name || 'world'
-    this.log(`hello ${name} from /Users/kimmo/git/meeshkan/http-types-kafka/src/commands/producer.ts`)
+    const name = flags.name || "world";
+    this.log(`hello ${name} from /Users/kimmo/git/meeshkan/http-types-kafka/src/commands/producer.ts`);
     if (args.file && flags.force) {
-      this.log(`you input --force and --file: ${args.file}`)
+      this.log(`you input --force and --file: ${args.file}`);
     }
   }
 }
